a84ba12fbe3c5bec1900b2271396e9fea6979af7
Rijk van Wel <rijk@uniti.be>
2020-10-29 09:23:00 +0100

    return to managed state

--- pages/index.js
+++ pages/index.js
@@ -62,61 +62,54 @@ function Home() {
 
                 <div className="relative">
                   <Menu>
-                    {({ open }) => (
-                      <>
-                        <Menu.Button className="p-2 transition duration-150 ease-in-out rounded-full hover:bg-blue-100 hover:text-blue-500 focus:bg-blue-100 focus:text-blue-500 focus:outline-none">
-                          <ShareIcon />
-                        </Menu.Button>
+                    <Menu.Button className="p-2 transition duration-150 ease-in-out rounded-full hover:bg-blue-100 hover:text-blue-500 focus:bg-blue-100 focus:text-blue-500 focus:outline-none">
+                      <ShareIcon />
+                    </Menu.Button>
 
-                        {open && (
-                          <Menu.Items
-                            as={motion.div}
-                            static
-                            initial={{ height: 0, opacity: 0 }}
-                            animate={{ height: "auto", opacity: 1 }}
-                            transition={{ duration: 0.15 }}
-                            className="absolute right-0 z-10 bg-white border rounded shadow focus:outline-none border-cool-gray-200"
+                    <Menu.Items
+                      as={motion.div}
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{ height: 'auto', opacity: 1 }}
+                      transition={{ duration: .4 }}
+                      className="overflow-hidden absolute right-0 z-10 bg-white border rounded shadow focus:outline-none border-cool-gray-200"
+                    >
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            className={`${
+                              active ? "bg-cool-gray-200" : ""
+                            } block px-5 py-3 whitespace-no-wrap`}
+                            href="#"
                           >
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  className={`${
-                                    active ? "bg-cool-gray-200" : ""
-                                  } block px-5 py-3 whitespace-no-wrap`}
-                                  href="#"
-                                >
-                                  Send via Direct Message
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  className={`${
-                                    active ? "bg-cool-gray-200" : ""
-                                  } block px-5 py-3 whitespace-no-wrap`}
-                                  href="#"
-                                >
-                                  Add Tweet to Bookmarks
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  className={`${
-                                    active ? "bg-cool-gray-200" : ""
-                                  } block px-5 py-3 whitespace-no-wrap`}
-                                  href="#"
-                                >
-                                  Copy link to Tweet
-                                </a>
-                              )}
-                            </Menu.Item>
-                          </Menu.Items>
+                            Send via Direct Message
+                          </a>
                         )}
-                      </>
-                    )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            className={`${
+                              active ? "bg-cool-gray-200" : ""
+                            } block px-5 py-3 whitespace-no-wrap`}
+                            href="#"
+                          >
+                            Add Tweet to Bookmarks
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            className={`${
+                              active ? "bg-cool-gray-200" : ""
+                            } block px-5 py-3 whitespace-no-wrap`}
+                            href="#"
+                          >
+                            Copy link to Tweet
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
                   </Menu>
                 </div>
               </div>
